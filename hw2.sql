b9b670d83839792ee59b452276b1123a49d9fc78
essentia2020 <kochetkovdm@hotmail.com>
2020-07-02 09:44:58 +0300

    Исправил проблему с кириллическими символами.

--- hw2.sql
+++ hw2.sql
@@ -1,10 +1,11 @@
+SET NAMES utf8;
 DROP DATABASE IF EXISTS example; 
 CREATE DATABASE example;
 USE example;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (id INT UNSIGNED 
 PRIMARY KEY AUTO_INCREMENT,
- name VARCHAR(255),
+ name VARCHAR(80),
  KEY index_of_name(name));
  INSERT INTO users VALUES
  (DEFAULT, 'Иванов М.В.'),
